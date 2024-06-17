0d99787089d5f6f7aa489b6949b683d9821e411e
mesteranas <135442710+mesteranas@users.noreply.github.com>
2024-06-17 14:05:28 +0300

    16/06/2024

---
+++ chatgpt/lib/app.dart
@@ -0,0 +1,7 @@
+class App{
+  static const name="chatgpt";
+  static const appName="chatgpt_flutter";
+  static const appDirName="chatgpt";
+  static const version=1.0;
+  static const description="a app to contect with chatgpt";
+}
